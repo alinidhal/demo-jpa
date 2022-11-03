7a2c24d090d29a4edba8dc0aa633e747c70c466d
Ali NIDHAL <ali.nidhal@hotmail.com>
2022-11-03 17:42:42 +0100

    Deuxième commit

--- src/main/java/fr/diginamic/ConnexionJpa.java
+++ src/main/java/fr/diginamic/ConnexionJpa.java
@@ -12,37 +12,60 @@ public class ConnexionJpa {
 	public static void main(String[] args) {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("recensement");
 		EntityManager em = factory.createEntityManager();
-		
+
 		EntityTransaction tr = em.getTransaction();
-		
+
 		tr.begin();
 		
-		Region region = new Region();
-		region.setNom("Bouche du Rhone");
-		region.setDateCreation(new Date());
-		em.persist(region);
-		
-		Ville ville = new Ville();
-		ville.setNom("Marseille");
-		ville.setCodePostal(13000);
-		ville.setDateDernierRecensement(new Date());
-		ville.setCategorie(Categorie.GRANDE);
-		ville.setRegion(region);
-		em.persist(ville);
-		
-		Habitant habitant = new Habitant();
-		habitant.setNom("Martin");
-		habitant.setPrenom("Raissa");
-		em.persist(habitant);
-		
-		ville.getHabitants().add(habitant);
-		
-		
+		//Creation d'un livre
+		Livre livre = new Livre();
+		em.persist(livre);
+//
+//		// Création d'une region
+//		Region region = new Region();
+//		region.setNom("Bouche du Rhone");
+//		region.setDateCreation(new Date());
+//		em.persist(region);
+//
+//		// Création d'une ville
+//		Ville marseille = new Ville();
+//		marseille.setNom("Marseille");
+//		marseille.setCodePostal(13001);
+//		marseille.setDateDernierRecensement(new Date());
+//		marseille.setCategorie(Categorie.GRANDE);
+//		marseille.setRegion(region);
+//		em.persist(marseille);
+//
+//		// Création d'une ville
+//		Ville aubagne = new Ville();
+//		aubagne.setNom("Aubagne");
+//		aubagne.setCodePostal(13400);
+//		aubagne.setDateDernierRecensement(new Date());
+//		aubagne.setCategorie(Categorie.MOYENNE);
+//		aubagne.setRegion(region);
+//		em.persist(aubagne);
+//
+//		// Création d'un habitant
+//		Habitant kassimeNidhal = new Habitant();
+//		kassimeNidhal.setNom("Kassime");
+//		kassimeNidhal.setPrenom("Nidhal");
+//		em.persist(kassimeNidhal);
+//
+//		// Ajout d'un habitant dans une ville
+//		marseille.getHabitants().add(kassimeNidhal);
+//
+//		Habitant martinRaissa = new Habitant();
+//		martinRaissa.setNom("Martin");
+//		martinRaissa.setPrenom("Raissa");
+//		em.persist(martinRaissa);
+//
+//		aubagne.getHabitants().add(martinRaissa);
+
 		tr.commit();
-		
-//		Region r1 = em.find(Region.class, 1);
-//		System.out.println(r1);
-		
+
+
+		Livre livre2 = em.find(Livre.class, 1);
+		System.out.println(livre2);
 	}
 
 }
